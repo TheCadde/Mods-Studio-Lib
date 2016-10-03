5e56922e4976059efabc189b40221e2e504caeab
Cadde <thecadde@gmail.com>
2016-10-03 12:48:55 +0200

    [ADD] Include debug files in the project. Namely a few example paint jobs and dumped units from ETS 2 ver 1.25. [CHANGE] Introduced ParseDumpUnits() in Sandbox project.

--- ModsStudioLib/MSLibSandbox/Program.cs
+++ ModsStudioLib/MSLibSandbox/Program.cs
@@ -9,21 +9,33 @@ namespace MSLibSandbox {
     [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
     internal static class Program {
         private static void Main(string[] args) {
-            if (args.Length == 0) {
-                try {
-                    var parser = new DumpedUnitsParser(@"C:\ETS2Units.txt");
-                    var result = parser.Parse();
-                } catch (Exception ex) {
-                    WriteLine($"{ex.Message}\n{ex.StackTrace}");
+            try {
+                if (args.Length == 0) {
+                    ParseDumpUnits();
                 }
+            } catch (Exception ex) {
+                WriteLine($"{ex.Message}\n{ex.StackTrace}");
             }
             WriteLine();
             WriteLine("Press any key...");
             ReadKey();
         }
 
+        private static void ParseDumpUnits() {
+            var parser = new DumpedUnitsParser(@"DebugFiles\Dumped units\ETS2Units 1.25.txt");
+            var result = parser.Parse();
+            foreach (var definitionStructureDescriptor in result.Values) {
+                WriteLine($"{definitionStructureDescriptor} {{");
+                foreach (var valueDescriptor in definitionStructureDescriptor.ValueDescriptors.Values) {
+                    WriteLine($"    {valueDescriptor}");
+                }
+                WriteLine("}");
+                WriteLine();
+            }
+        }
+
         private static void PrintStructures() {
-            var parser = new DefinitionFileParser(@"G:\ETS 2 extracts\extracts 1.25.1.2\def\vehicle\truck\volvo.fh16\paint_job\color_m1.dlc_metallics2.sii");
+            var parser = new DefinitionFileParser(@"DebugFiles\Definitions\Paintjobs\scania.r\color_m1.dlc_metallics2.sii");
             var structures = parser.Parse();
 
             foreach (var definitionStructure in structures) {
